5c91557741a1049968deca00484bea684641ca98
iperez <iperez@CLAP1704-1105.ids.com.mx>
2018-09-26 21:01:14 -0500

    Se agrega implementación para el servicio del catálogo de puestos

---
+++ RecursosHumanosCenicco/src/main/java/com/lam/recursoshumanoscenicco/service/PuestoService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.recursoshumanoscenicco.service;
+
+import com.lam.recursoshumanoscenicco.exception.ServiceException;
+import com.lam.recursoshumanoscenicco.model.Puesto;
+import java.util.List;
+
+/**
+ *
+ * @author iperez
+ */
+public interface PuestoService extends GenericService<Puesto, Long>{
+    
+    public List<Puesto> findPuestos() throws ServiceException;
+    
+}
