ae5b1b3bb6258bb91ad77da8de62364bc07752c3
iperez <iperez@CLAP1704-1105.ids.com.mx>
2018-10-11 15:20:10 -0500

    Se agregan mapeos con la entidades que se relacionan en el guardado del examen con configuraciones correctas

--- RecursosHumanosCenicco/src/test/java/com/lam/humanresources/test/catalogs/ExamenCatalogoTest.java
+++ RecursosHumanosCenicco/src/test/java/com/lam/humanresources/test/catalogs/ExamenCatalogoTest.java
@@ -25,52 +25,53 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author iperez
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "file:src/main/resources/applicationcontext.xml" })
+@ContextConfiguration(locations = {"file:src/main/resources/applicationcontext.xml"})
 public class ExamenCatalogoTest {
-    
+
     @Autowired
     private ExamenService examenService;
-    
+
     @Autowired
     private PuestoService puestoService;
-    
+
     @Autowired
     private CatalogoParametroService catalogoParametroService;
-    
+
     @Test
     public void test() throws ServiceException {
         System.out.println("Estoy en el test:::::");
         Examen examen = new Examen();
-        examen.setDescripcion("Net");
-        examen.setDuracion("3");
+        examen.setDescripcion("Agile LEAD");
+        examen.setDuracion("1");
         examen.setCatalogoParametro(catalogoParametroService.buscarPorId(4L));
         examen.setPuesto(puestoService.buscarPorId(3L));
         examen.setPreguntas(this.armarPreguntas());
-        
         Long id = examenService.guardar(examen);
         System.out.println("DEBUG IIPG --> El id del examen" + id);
-        
+
     }
-    
+
     private Set<Pregunta> armarPreguntas() throws ServiceException {
         Set<Pregunta> preguntas = new HashSet<>();
         Pregunta preguntaA = new Pregunta();
-        preguntaA.setDescripcion("pregunta aaaaaa");
+        preguntaA.setDescripcion("pregunta una");
         preguntaA.setCatalogoParametro(catalogoParametroService.buscarPorId(1L));
-        preguntaA.setRespuestas(this.armarRespuestas(preguntaA));
+        Set<Respuesta> respuestas = armarRespuestas(preguntaA);
+        preguntaA.getRespuestas().addAll(respuestas);
+        System.out.println("DEBUG IIPG --> cuantas respuestas: " + preguntaA.getRespuestas().size());
         preguntas.add(preguntaA);
         return preguntas;
     }
-    
+
     private Set<Respuesta> armarRespuestas(Pregunta pregunta) {
+        System.out.println("Estoy agregando las respuestas:::::::::");
         Set<Respuesta> respuestas = new HashSet<>();
         Respuesta respuesta = new Respuesta();
         respuesta.setPregunta(pregunta);
-        respuesta.setDescripcion("Respuesta uno");
+        respuesta.setDescripcion("Respuesta correcta");
         respuesta.setEstado("1");
         respuestas.add(respuesta);
-        
         return respuestas;
     }
-    
+
 }
